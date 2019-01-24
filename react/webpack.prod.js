45b6b038176b41c9698ec4975ba24d60fdbd31ed
HarveyQiu <qiushimao@hotmail.com>
2019-01-24 11:27:05 +0800

    add production mode

--- react/webpack.prod.js
+++ react/webpack.prod.js
@@ -2,6 +2,7 @@ const merge = require('webpack-merge');
 const config = require('./webpack.config');
 
 module.exports = merge(config, {
+    mode: "production",
     output: {
         filename: '[name].[hash].js',
         path: path.resolve(__dirname, 'dist')
