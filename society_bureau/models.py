a85cb6b5c29d536672ca5db2a818d27e5205316a
Ni_Chujie <ncj19991213@126.com>
2019-01-26 15:07:32 +0800

    update society_bureau/models

--- society_bureau/models.py
+++ society_bureau/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
+from student.models import Student
+
 
 # Create your models here.
+
+
+class SocietyBureau(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    real_name = models.CharField(max_length=64)
+    qq = models.CharField(max_length=32, blank=True)
+    email = models.EmailField(blank=True)
+    password_changed = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.real_name
