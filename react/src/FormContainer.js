2661f7da0a8b85eef2a43d3121f6e36de3772716
Harvey Qiu <qiushimao@hotmail.com>
2019-01-27 17:58:11 +0800

    add react quill (#12)
    * add react quill
    
    * remove quill delta

--- react/src/FormContainer.js
+++ react/src/FormContainer.js
@@ -1,15 +1,40 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
 
 class FormContainer extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-    };
-  }
-  render() {
-    return (
-      <div>233</div>
-    );
-  }
+    constructor(props) {
+        super(props);
+        this.state = {text: ''};
+    }
+
+    handleChange(value) {
+        this.setState({text: value})
+    }
+
+    render() {
+        const modules = {
+            toolbar: [
+                [{'header': [1, 2, 3, 4, 5, false]}],
+                ['bold', 'italic', 'underline', 'strike', 'blockquote'],
+                [{'list': 'ordered'}, {'list': 'bullet'}, {'indent': '-1'}, {'indent': '+1'}],
+                ['link', 'image'],
+                ['clean']
+            ],
+        };
+
+        const formats = [
+            'header',
+            'bold', 'italic', 'underline', 'strike', 'blockquote',
+            'list', 'bullet', 'indent',
+            'link', 'image'
+        ];
+
+        return (
+            <ReactQuill value={this.state.text} modules={modules} formats={formats}
+                        onChange={(value) => this.handleChange(value)}/>
+        )
+    }
 }
+
 export default FormContainer;
