d77cd05046e5d54920a8e104cc3ebebb4d5983ce
Ni_Chujie <ncj19991213@126.com>
2019-01-28 22:30:17 +0800

    create change_password form

--- react/src/app/home/index.js
+++ react/src/app/home/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import {Switch, Route} from 'react-router-dom';
 import StudentLogin from './containers/StudentLogin';
+import WrappedChangePasswordForm from '../../shared/change_password/ChangePasswordForm';
 
 export default function Home({match}) {
     return (
         <Switch>
             <Route path={`${match.url}login`} component={StudentLogin}/>
             <Route path={`${match.url}`} exact component={function () {
-                return <h1>Index</h1>
+                return <WrappedChangePasswordForm/>;
             }}/>
         </Switch>
     );
