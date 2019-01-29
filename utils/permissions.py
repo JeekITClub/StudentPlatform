29d0298b5b607ca062d95ac9f5678324749f7f04
HarveyQiu <qiushimao@hotmail.com>
2019-01-29 16:42:23 +0800

    add more permissions

--- utils/permissions.py
+++ utils/permissions.py
@@ -5,7 +5,28 @@ class IsStudent(permissions.BasePermission):
     message = 'Student Only'
 
     def has_permission(self, request, view):
-        return request.user.is_authenticated
+        return request.user.is_authenticated and hasattr(request.user, 'student')
 
     def has_object_permission(self, request, view, obj):
         return request.user == obj.user
+
+
+class JoinSociety(IsStudent):
+    message = '不能加入社团'
+
+    def has_object_permission(self, request, view, obj):
+        return request.user.student not in obj.members
+
+
+class AccessSocietyAdmin(permissions.BasePermission):
+    message = 'Society Only'
+
+    def has_permission(self, request, view):
+        return request.user.is_authenticated and hasattr(request.user, 'society')
+
+
+class AccessSocietyBureauAdmin(permissions.BasePermission):
+    message = 'Society Bureau Only'
+
+    def has_permission(self, request, view):
+        return request.user.is_authenticated and hasattr(request.user, 'society_bureau')
