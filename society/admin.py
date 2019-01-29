96467edc928951f89d2667a22e54b1a025a928d4
Harvey Qiu <qiushimao@hotmail.com>
2019-01-29 14:12:51 +0800

    alter name of SMRS & remove through (#21)
    * push
    
    * remove test

--- society/admin.py
+++ society/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from society.models import Society, SocietyMemberRelationShip
+from society.models import Society, JoinSocietyRequest
 
 
 # Register your models here.
@@ -8,9 +8,9 @@ class SocietyAdmin(admin.ModelAdmin):
     list_display = ('society_id', 'name', 'confirmed', 'recruit')
 
 
-class SocietyMemberRelationShipAdmin(admin.ModelAdmin):
+class JoinSocietyRequestAdmin(admin.ModelAdmin):
     list_display = ('society', 'member', 'status')
 
 
 admin.site.register(Society, SocietyAdmin)
-admin.site.register(SocietyMemberRelationShip, SocietyMemberRelationShipAdmin)
+admin.site.register(JoinSocietyRequest, JoinSocietyRequestAdmin)
