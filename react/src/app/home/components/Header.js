7e3757549ff437cb1f98bb36cda0c7fab2a6e062
Harvey Qiu <qiushimao@hotmail.com>
2019-02-01 12:35:31 +0800

    update (#28)

--- react/src/app/home/components/Header.js
+++ react/src/app/home/components/Header.js
@@ -1,10 +1,8 @@
 import React from 'react';
 
 class Header extends React.Component {
-    render() {
-        return (
-            <div>Header</div>
-        )
+    render () {
+
     }
 }
 
--- react/src/router.js
+++ react/src/router.js
@@ -1,13 +1,11 @@
 import React from 'react';
-import {BrowserRouter, Route, HashRouter, Switch} from 'react-router-dom';
-import FormContainer from "./FormContainer";
+import {Route, HashRouter, Switch} from 'react-router-dom';
 import Home from './app/home/index.js';
 import Society from './app/society/index.js';
 import AdminSociety from './app/admin_society/index.js';
-import Header from './shared/header/index.js';
 
-const DEV = process.env.NODE_ENV !== 'production';
-const DEBUG = process.env.DEBUG === 'true';
+// const DEV = process.env.NODE_ENV !== 'production';
+// const DEBUG = process.env.DEBUG === 'true';
 // const MyRouter = (DEV && !DEBUG) ? HashRouter : BrowserRouter;
 const MyRouter = HashRouter;
 
@@ -18,8 +16,8 @@ class AppRouter extends React.Component {
                 <div style={{height: '100%'}}>
                     <Switch>
                         <Route path="/society" component={Society}/>
-                        <Route path="/admin_society/" component={AdminSociety}/>
-                        <Route path="/" component={Home}/>
+                        <Route path="/admin_society" component={AdminSociety}/>
+                        <Route path="" component={Home}/>
                     </Switch>
                 </div>
             </MyRouter>
