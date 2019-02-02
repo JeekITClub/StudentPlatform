9afd9b2b55340993dc2a2255a3fc9dee1bf5564e
Ni_Chujie <ncj19991213@126.com>
2019-02-03 00:47:00 +0800

    update menus

--- react/src/app/society_bureau/components/SocietyBureauSider.js
+++ react/src/app/society_bureau/components/SocietyBureauSider.js
@@ -1,46 +1,40 @@
 import React from 'react';
 import {Menu, Icon} from 'antd';
+import {withRouter} from "react-router-dom";
 
 
 class SocietyBureauSider extends React.Component {
+    handleClick = (e) => {
+        console.log('click ', e);
+        this.props.history.push(e.key);
+    };
+
     render() {
         return (
-            <Menu theme="dark" mode="vertical">
-                <Menu.Item key="1">
-                    <Icon type="user"/>
-                    <span className="nav-text">nav 1</span>
+            <Menu theme="dark" mode="inline" onClick={this.handleClick}>
+                <Menu.Item key="/manage">
+                    <Icon type="dashboard"/>
+                    <span>Dashboard</span>
                 </Menu.Item>
-                <Menu.Item key="2">
-                    <Icon type="video-camera"/>
-                    <span className="nav-text">nav 2</span>
-                </Menu.Item>
-                <Menu.Item key="3">
-                    <Icon type="upload"/>
-                    <span className="nav-text">nav 3</span>
-                </Menu.Item>
-                <Menu.Item key="4">
-                    <Icon type="bar-chart"/>
-                    <span className="nav-text">nav 4</span>
-                </Menu.Item>
-                <Menu.Item key="5">
-                    <Icon type="cloud-o"/>
-                    <span className="nav-text">nav 5</span>
-                </Menu.Item>
-                <Menu.Item key="6">
-                    <Icon type="appstore-o"/>
-                    <span className="nav-text">nav 6</span>
-                </Menu.Item>
-                <Menu.Item key="7">
+                <Menu.Item key="/manage/society">
                     <Icon type="team"/>
-                    <span className="nav-text">nav 7</span>
+                    <span>社团管理</span>
                 </Menu.Item>
-                <Menu.Item key="8">
-                    <Icon type="shop"/>
-                    <span className="nav-text">nav 8</span>
+                <Menu.Item key="/manage/audit">
+                    <Icon type="edit"/>
+                    <span>社团审核</span>
+                </Menu.Item>
+                <Menu.Item key="/manage/credit">
+                    <Icon type="star"/>
+                    <span>学分管理</span>
+                </Menu.Item>
+                <Menu.Item key="/manage/message">
+                    <Icon type="mail"/>
+                    <span>收件箱</span>
                 </Menu.Item>
             </Menu>
         )
     }
 }
 
-export default SocietyBureauSider;
+export default withRouter(SocietyBureauSider);
