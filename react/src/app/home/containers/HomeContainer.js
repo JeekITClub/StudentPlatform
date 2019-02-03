059d60e05ed1995c1b3a6528d4cf1db586273a14
HarveyQiu <qiushimao@hotmail.com>
2019-02-03 16:22:52 +0800

    update home

--- react/src/app/home/containers/HomeContainer.js
+++ react/src/app/home/containers/HomeContainer.js
@@ -1,17 +1,31 @@
 import React from 'react';
-
+import {Link} from 'react-router-dom';
 import '../styles/Home.scss'
 
 class HomeContainer extends React.Component {
     render() {
         return (
-            <div className="container-fluid">
-                <div className="block-1">
-
-                </div>
-                {/*<div className="text">*/}
-                    {/*J*/}
-                {/*</div>*/}
+            <div className="container-fluid home-container">
+                <ul className="home-header">
+                    <div className="left">
+                        <li className="nav-item">
+                            <Link to={'/'} className="nav-link brand">JPSP</Link>
+                        </li>
+                    </div>
+                    <div className="center">
+                        <li className="nav-item">
+                            <Link to={'/'} className="nav-link">社团</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link to={'/'} className="nav-link">帮助</Link>
+                        </li>
+                    </div>
+                    <div className="right">
+                        <li className="nav-item">
+                            <Link to={'/'} className="nav-link">登陆</Link>
+                        </li>
+                    </div>
+                </ul>
             </div>
         )
     }
