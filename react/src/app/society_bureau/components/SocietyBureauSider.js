1f4e0eaaddda8a43b8c23f82cf735a82d716af8b
Harvey Qiu <qiushimao@hotmail.com>
2019-02-03 09:29:26 +0800

    update manage styles (#33)

--- react/src/app/society_bureau/components/SocietyBureauSider.js
+++ react/src/app/society_bureau/components/SocietyBureauSider.js
@@ -1,46 +1,50 @@
 import React from 'react';
 import {Menu, Icon} from 'antd';
+import {withRouter} from "react-router-dom";
+import PropTypes from "prop-types";
 
 
+@withRouter
 class SocietyBureauSider extends React.Component {
+    state = {
+        key: ['dashboard']
+    };
+    handleOnClick = (e) => {
+        this.setState({key: [e.key]});
+        this.props.history.push(e.key);
+    };
+
     render() {
         return (
-            <Menu theme="dark" mode="vertical" defaultSelectedKeys={['4']}>
-                <Menu.Item key="1">
+            <Menu
+                multiple={false}
+                onClick={this.handleOnClick}
+                theme="dark"
+                mode="vertical"
+                defaultSelectedKeys={['dashboard']}
+                selectedKeys={this.state.key}
+            >
+                <Menu.Item key="dashboard">
+                    <Icon type="dashboard"/>
+                    仪表盘
+                </Menu.Item>
+                <Menu.Item key="society">
+                    <Icon type="reconciliation"/>
+                    <span className="nav-text">社团</span>
+                </Menu.Item>
+                <Menu.Item key="student-user">
                     <Icon type="user"/>
-                    <span className="nav-text">nav 1</span>
-                </Menu.Item>
-                <Menu.Item key="2">
-                    <Icon type="video-camera"/>
-                    <span className="nav-text">nav 2</span>
-                </Menu.Item>
-                <Menu.Item key="3">
-                    <Icon type="upload"/>
-                    <span className="nav-text">nav 3</span>
-                </Menu.Item>
-                <Menu.Item key="4">
-                    <Icon type="bar-chart"/>
-                    <span className="nav-text">nav 4</span>
-                </Menu.Item>
-                <Menu.Item key="5">
-                    <Icon type="cloud-o"/>
-                    <span className="nav-text">nav 5</span>
-                </Menu.Item>
-                <Menu.Item key="6">
-                    <Icon type="appstore-o"/>
-                    <span className="nav-text">nav 6</span>
-                </Menu.Item>
-                <Menu.Item key="7">
-                    <Icon type="team"/>
-                    <span className="nav-text">nav 7</span>
-                </Menu.Item>
-                <Menu.Item key="8">
-                    <Icon type="shop"/>
-                    <span className="nav-text">nav 8</span>
+                    <span className="nav-text">学生用户</span>
                 </Menu.Item>
             </Menu>
         )
     }
 }
+
+SocietyBureauSider.propTypes = {
+    match: PropTypes.object,
+    location: PropTypes.object,
+    history: PropTypes.object
+};
 
 export default SocietyBureauSider;
