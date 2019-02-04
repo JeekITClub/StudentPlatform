9afb05eb720068aeb79b96d79382328b73f63a27
Ni_Chujie <ncj19991213@126.com>
2019-02-04 23:11:33 +0800

    update django admins

--- society_bureau/admin.py
+++ society_bureau/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from society_bureau.models import SocietyBureau
+from society_bureau.models import SocietyBureau, SiteSettings
 
 # Register your models here.
 
 admin.site.register(SocietyBureau)
+admin.site.register(SiteSettings)
