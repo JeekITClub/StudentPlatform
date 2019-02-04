9bd2684242f0d5b53fdf2fc41836663fe379cb43
Ni_Chujie <ncj19991213@126.com>
2019-02-05 00:51:43 +0800

    update models

--- society/models.py
+++ society/models.py
@@ -20,7 +20,7 @@ class SocietyTag(models.Model):
 
 class Society(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    members = models.ManyToManyField(Student)
+    members = models.ManyToManyField(Student, blank=True, null=True)
     society_id = models.PositiveIntegerField(null=True, blank=True, unique=True)
     name = models.CharField(max_length=64)
     introduction = models.TextField(blank=True)
@@ -37,7 +37,7 @@ class Society(models.Model):
     established_time = models.DateTimeField(blank=True, null=True)
     password_changed = models.BooleanField(default=False)
     credit = models.PositiveSmallIntegerField(default=0)
-    tags = models.ManyToManyField(SocietyTag)
+    tags = models.ManyToManyField(SocietyTag, blank=True, null=True)
 
     class Meta:
         ordering = ['-established_time']
@@ -50,6 +50,8 @@ class JoinSocietyRequest(models.Model):
     society = models.ForeignKey(Society, on_delete=models.DO_NOTHING)
     member = models.ForeignKey(Student, on_delete=models.DO_NOTHING)
     status = models.PositiveSmallIntegerField(choices=member_confirm_status, default=JoinSocietyRequestStatus.WAITING)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return str(self.society) + ' ' + str(self.member)
@@ -62,6 +64,8 @@ class ActivityRequest(models.Model):
     place = models.CharField(max_length=32)
     start_time = models.DateTimeField()
     status = models.PositiveSmallIntegerField(choices=activity_confirm_status, default=ActivityRequestStatus.WAITING)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return str(self.society) + ' ' + self.title
