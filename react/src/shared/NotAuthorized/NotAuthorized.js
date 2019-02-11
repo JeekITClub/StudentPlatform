ed1334d5d8422ed1f607120b7ebae48e4ad25354
HarveyQiu <qiushimao@hotmail.com>
2019-02-11 11:55:55 +0800

    add society required route

--- react/src/shared/NotAuthorized/NotAuthorized.js
+++ react/src/shared/NotAuthorized/NotAuthorized.js
@@ -1,9 +1,13 @@
 import React from 'react';
 
+import './NotAuthorized.scss'
+
 class NotAuthorized extends React.Component {
     render () {
         return (
-            <div>你可没有权限呢</div>
+            <div className="not-authorized-container">
+                <p className="not-authorized-text">你可没有权限呢</p>
+            </div>
         )
     }
 }
