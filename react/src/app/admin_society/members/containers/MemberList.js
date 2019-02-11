a16edc20c6965b11930162efae6e62d94033dd9a
Harvey Qiu <qiushimao@hotmail.com>
2019-02-11 11:40:08 +0800

    kick members (#56)
    * kick members
    
    * fix
    
    * fix
    
    * fix

--- react/src/app/admin_society/members/containers/MemberList.js
+++ react/src/app/admin_society/members/containers/MemberList.js
@@ -1,34 +1,76 @@
 import React from 'react';
 
-import { Link } from 'react-router-dom';
-import { Empty, Table } from 'antd'
+import {Table, Button} from 'antd'
 
-const { Column } = Table;
+import Provider from '../../../../utils/provider'
 
 class MemberList extends React.Component {
     state = {
-        members: [{name: '123', grade: '1', class_num: '1', student_id: 1}]
+        members: [{name: '2', grade: 1, class_num: 1, student_id: 233}]
+    };
+
+    componentDidMount() {
+        Provider.get('/api/society_manage/member/')
+            .then((res) => {
+                this.setState({members: res.data})
+            })
+            .catch((err) => {
+              console.log(err)
+            })
+    }
+
+    handleKickMember = (member_id) => {
+        Provider.post('/api/society_manage/member/kick', {
+            member_id: member_id
+        })
+            .then((res) => {
+                console.log(res.status)
+            })
+            .catch((err) => {
+                console.log(err)
+            })
     };
 
     renderMemberList = () => {
+        const columns = [
+            {
+                title: '姓名',
+                dataIndex: 'name',
+                key: 'name'
+            },
+            {
+                title: '年级',
+                dataIndex: 'grade',
+                key: 'grade'
+            },
+            {
+                title: '班级',
+                dataIndex: 'class_num',
+                key: 'class_num'
+            },
+            {
+                title: '详情',
+                key: 'detail'
+            },
+            {
+                title: '操作',
+                key: 'operation',
+                dataIndex: 'id',
+                render: (text) => {
+                    return (<Button onClick={() => this.handleKickMember(text)}>踢出成员</Button>)
+                }
+            }
+        ];
+
         return (
-            <Table dataSource={this.state.members}>
-                <Column title="姓名" dataIndex="name" key="name" />
-                <Column title="年级" dataIndex="grade" key="grade" />
-                <Column title="班级" dataIndex="class_num" key="class_num" />
-                <Column title="详情" dataIndex="student_id" key="detail" render={
-                    student_id => (
-                       <Link to={`/admin_society/member/${student_id}/`}>{student_id}</Link>
-                    )
-                } />
-            </Table>
+            <Table dataSource={this.state.members} columns={columns} rowKey="student_id"/>
         )
     };
 
-    render () {
-      return (
-          this.state.members.length === 0 ? <Empty /> : this.renderMemberList()
-      )
+    render() {
+        return (
+            this.renderMemberList()
+        )
     }
 }
 
