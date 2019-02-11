bb3b9f1359a737c9b6ac53e2777148784676b446
HarveyQiu <qiushimao@hotmail.com>
2019-02-11 17:18:20 +0800

    fix

--- utils/filters.py
+++ utils/filters.py
@@ -6,9 +6,15 @@ class NameFilterBackend(filters.BaseFilterBackend):
     Filter that only allows users to see their own objects.
     """
     def filter_queryset(self, request, queryset, view):
-        return queryset.filter(name__contains=request.query_params['name'])
+        name = request.query_params.get('name', None)
+        if name:
+            return queryset.filter(name__contains=name)
+        return queryset
 
 
 class StatusFilterBackend(filters.BaseFilterBackend):
     def filter_queryset(self, request, queryset, view):
-        return queryset.filter(status=request.query_params['status'])
+        status = request.query_params.get('status', None)
+        if status:
+            return queryset.filter(status=status)
+        return queryset
