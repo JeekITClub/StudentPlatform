3e309797f67970e6677e5a67a993f6f97fbde906
HarveyQiu <qiushimao@hotmail.com>
2019-02-13 21:33:24 +0800

    initial commit

---
+++ react/src/app/admin_society/page/components/PageCustomizeDrawerMenu.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {Drawer} from "antd";
+
+class PageCustomizeDrawerMenu extends React.Component {
+    render () {
+        return (
+            <Drawer mask={false} visible={true} placement="left">
+                <p>233</p>
+
+            </Drawer>
+        )
+    }
+}
+
+export default PageCustomizeDrawerMenu;
--- react/src/app/admin_society/router.js
+++ react/src/app/admin_society/router.js
@@ -3,12 +3,14 @@ import {Route, Switch} from "react-router-dom";
 
 import Member from "./members";
 import Credit from "./credit";
+import PageCustomizeDrawerMenu from "./page/components/PageCustomizeDrawerMenu";
 
 export default function AdminSocietyRouter({match}) {
     return (
         <Switch>
             <Route path={`${match.url}/member`} component={Member}/>
             <Route path={`${match.url}/credit`} component={Credit}/>
+            <Route path={`${match.url}/page`} component={PageCustomizeDrawerMenu}/>
         </Switch>
     )
 }
