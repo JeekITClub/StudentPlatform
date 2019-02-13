5b6fe3b37cce8aaf40a87be1b7417ed3beba7d21
HarveyQiu <qiushimao@hotmail.com>
2019-02-13 19:26:08 +0800

    add operation

--- react/src/app/admin_society/join_request/components/JoinRequestList.js
+++ react/src/app/admin_society/join_request/components/JoinRequestList.js
@@ -1,14 +1,27 @@
 import React from 'react';
 
-import {Table} from 'antd';
+import {Table, Button} from 'antd';
 
 import Provider from '../../../../utils/provider'
 
 class JoinRequestList extends React.Component {
+    state = {
+        join_requests: [
+            {
+                id: 1,
+                member: {
+                    grade: 1,
+                    class_num: 1,
+                    name: 'ncj'
+                }
+            }
+        ]
+    };
+
     componentDidMount() {
         Provider.get('/api/society_manage/join_request/')
             .then((res) => {
-                console.log(res)
+                this.setState({join_request: res.data})
             })
             .catch((err) => {
                 console.log(err)
@@ -18,12 +31,25 @@ class JoinRequestList extends React.Component {
     render () {
         const columns = [
             {
-
+                title: '申请加入者信息',
+                dataIndex: 'member',
+                key: 'member_information',
+                render: (item) => {
+                    return `${item.grade}-${item.class_num}`
+                }
+            },
+            {
+                title: '操作',
+                render: () => {
+                    return (
+                        <Button htmlType="button">按钮</Button>
+                    )
+                }
             }
         ];
 
         return (
-            <Table columns={columns} rowKey="id" />
+            <Table columns={columns} rowKey="id" dataSource={this.state.join_requests}/>
         )
     }
 }
--- react/src/app/admin_society/router.js
+++ react/src/app/admin_society/router.js
@@ -3,12 +3,14 @@ import {Route, Switch} from "react-router-dom";
 
 import Member from "./members";
 import Credit from "./credit";
+import JoinRequestList from "./join_request/components/JoinRequestList";
 
 export default function AdminSocietyRouter({match}) {
     return (
         <Switch>
             <Route path={`${match.url}/members`} component={Member}/>
             <Route path={`${match.url}/credit`} component={Credit}/>
+            <Route path={`${match.url}/join_request`} component={JoinRequestList} />
         </Switch>
     )
 }
