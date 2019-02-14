b4edcbe65a6ac2a364e55fa170b08d5ccda26da3
HarveyQiu <qiushimao@hotmail.com>
2019-02-14 17:16:50 +0800

    update models

--- society_manage/models.py
+++ society_manage/models.py
@@ -2,17 +2,18 @@ from django.db import models
 from society.models import Society, Student
 
 
-class CreditReceivers(models.Model):
-    society = models.ForeignKey(Society, on_delete=models.DO_NOTHING, related_name='credit_receivers')
-    receivers = models.ManyToManyField(Student, blank=True, related_name='credit_givers')
+class CreditDistribution(models.Model):
+    society = models.ForeignKey(Society, on_delete=models.DO_NOTHING, related_name='distributed_credit')
+    receivers = models.ManyToManyField(Student, blank=True, related_name='receive_credit_from', default=None)
     year = models.PositiveSmallIntegerField()
     semester = models.PositiveSmallIntegerField()
+    credit = models.PositiveSmallIntegerField(help_text='可获得学分的人数的最大值')
 
     class Meta:
         unique_together = ("society", "year", "semester")
 
     def __str__(self):
-        return str(self.society)
+        return self.society.name
 
     @property
     def count(self):
