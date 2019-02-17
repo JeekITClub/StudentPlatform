e6550c7ca2c4ed9e2b00a892affd88db1818d3f5
Ni_Chujie <ncj19991213@126.com>
2019-02-17 13:58:39 +0800

    Revert "update route"
    This reverts commit a390f81285f4f85091bfb0f4b06515560c607bd0.

--- react/src/router.js
+++ react/src/router.js
@@ -30,7 +30,7 @@ class AppRouter extends React.Component {
             <MyRouter>
                 <div style={{height: '100%'}}>
                     <Switch>
-                        <LoginRequiredRoute path="/manage" component={SocietyBureau}/>
+                        <Route path="/manage" component={SocietyBureau}/>
                         <LoginRequiredRoute path="/admin_society" component={AdminSociety}/>
                         <Route path="" component={Home}/>
                     </Switch>
