e85c102ac223d66ab155b4eee011b8ac0f37ad56
Ni_Chujie <ncj19991213@126.com>
2019-02-17 19:07:40 +0800

    add NotLogin Component

---
+++ react/src/shared/NotLogin/NotLogin.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+import {Row, Col} from 'antd';
+
+import './NotLogin.scss';
+
+class NotLogin extends React.Component {
+    render() {
+        return (
+            <Row type="flex" align="middle" justify="center" className="not-login-container">
+                <Row>
+                    <Col span={24}>
+                        <p className="not-login-text">你还没有登录</p>
+                    </Col>
+                    <Col span={12}>
+                        <h4 className="not-login-link">
+                            <Link to={'/'}>回首页</Link>
+                        </h4>
+                    </Col>
+                    <Col span={12}>
+                        <h4 className="not-login-link">
+                            <Link to={'/login'}>去登录</Link>
+                        </h4>
+                    </Col>
+                </Row>
+            </Row>
+        )
+    }
+}
+
+export default NotLogin;
