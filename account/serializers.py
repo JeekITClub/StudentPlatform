36ee74871b325e1c126924b31c4605a0b7ce3b7b
Harvey Qiu <qiushimao@hotmail.com>
2019-02-19 16:29:44 +0800

    better identity (#106)
    * better identity
    
    * update

--- account/serializers.py
+++ account/serializers.py
@@ -1,6 +1,9 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
 
+from society.models import Society
+from student.models import Student
+
 
 class LoginSerializer(serializers.Serializer):
     username = serializers.CharField()
@@ -18,18 +21,44 @@ class UserDetailSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ('identity', 'password_changed',)
+        fields = ('id', 'username', 'identity', 'password_changed',)
 
     def get_identity(self, obj):
         if hasattr(obj, 'student'):
-            return 'student'
+            return StudentIdentitySerializer(obj.student).data
         elif hasattr(obj, 'society'):
-            return 'society'
+            return SocietyIdentitySerializer(obj.society).data
         elif hasattr(obj, 'society_bureau'):
-            return 'society_bureau'
-        return 'who are u?'
+            return {
+                'identity': 'society_bureau'
+            }
+        return {
+            'identity': 'who are u?'
+        }
 
     def get_password_changed(self, obj):
         if obj.check_password('123456'):
             return False
         return True
+
+
+class SocietyIdentitySerializer(serializers.ModelSerializer):
+    identity = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Society
+        fields = ('id', 'name', 'identity')
+
+    def get_identity(self, obj):
+        return 'society'
+
+
+class StudentIdentitySerializer(serializers.ModelSerializer):
+    identity = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Student
+        fields = ('id', 'name', 'identity')
+
+    def get_identity(self, obj):
+        return 'student'
