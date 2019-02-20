7a33d8508a5a6f64f95b86c7fbb4bbf8d33b38de
Ni_Chujie <ncj19991213@126.com>
2019-02-20 19:39:43 +0800

    update 404

--- react/src/shared/NotFound/NotFound.js
+++ react/src/shared/NotFound/NotFound.js
@@ -6,9 +6,11 @@ import {Row, Col} from 'antd';
 class NotFound extends React.Component {
     render() {
         return (
-            <Row type="flex" align="middle" justify="center">
-                <Col>
-                    <h2>你来到了未知地带...</h2>
+            <Row type="flex" align="middle" justify="center" className="mt-5">
+                <Col className="text-center">
+                    <h1>404 Not Found</h1>
+                    <h4>😲 你要查看的内容不见了 😲</h4>
+                    <h4><Link to={'/'}>回首页</Link></h4>
                 </Col>
             </Row>
         )
