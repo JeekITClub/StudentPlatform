bfa4985f851627b7401b51e763f677b6e34f362f
Ni_Chujie <ncj19991213@126.com>
2019-02-21 12:03:51 +0800

    add little game

--- react/src/shared/NotFound/NotFound.js
+++ react/src/shared/NotFound/NotFound.js
@@ -1,20 +1,24 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 import {Row, Col} from 'antd';
-
+import Game from '../TRexRunner/TRexRunner';
 
 class NotFound extends React.Component {
     render() {
         return (
-            <Row type="flex" align="middle" justify="center" className="mt-5">
-                <Col className="text-center">
-                    <h1>404 Not Found</h1>
-                    <h4>😲 你要查看的内容不见了 😲</h4>
-                    <h4 className="mt-5"><Link to={'/'}>回首页</Link></h4>
-                    <p>或者</p>
-                    <h4><a href='https://github.com/JeekStudio'>加入我们</a></h4>
-                </Col>
-            </Row>
+            <div>
+                <Game/>
+                <Row type="flex" align="middle" justify="center" className="mt-5">
+                    <Col className="text-center">
+                        <h1>404 Not Found</h1>
+                        <h4>😲 你要查看的内容不见了 😲</h4>
+                        <h4>这里只有一只小恐龙</h4>
+                        <h4 className="mt-5"><Link to={'/'}>回首页</Link></h4>
+                        <p>或者</p>
+                        <h4><a href='https://github.com/JeekStudio'>加入我们</a></h4>
+                    </Col>
+                </Row>
+            </div>
         )
     }
 }
