73a1f27743c853c332a4d2b431da875bb0ef7a7b
HarveyQiu <qiushimao@hotmail.com>
2019-02-23 12:30:51 +0800

    remove deprecated sb layout

--- react/src/app/society_bureau/index.js
+++ react/src/app/society_bureau/index.js
@@ -11,6 +11,8 @@ import './styles/index.scss';
 import AccountStore from '../../shared/stores/AccountStore';
 import NotAuthorized from "../../shared/NotAuthorized/NotAuthorized";
 
+import AdminLayout from '../../shared/AdminLayout/AdminLayout'
+
 const {
     Footer
 } = Layout;
@@ -18,29 +20,7 @@ const {
 export default function SocietyBureau({match}) {
     return (
         AccountStore.is_society_bureau ?
-            <div>
-                <SBDrawerMenu/>
-                <Row className="society-bureau-container">
-                    <Col xs={0} sm={0} md={0} lg={4} xl={3} className="society-bureau-sider-container">
-                        <SocietyBureauSider/>
-                    </Col>
-                    <Col xs={24} sm={24} md={24} lg={20} xl={21} className="society-bureau-main-wrapper">
-                        <Row>
-                            <SocietyBureauHeader/>
-                        </Row>
-                        <Row className="container-fluid mt-4">
-                            <SBRouter match={match}/>
-                        </Row>
-                    </Col>
-                </Row>
-                <Layout>
-                    <Footer>
-                        <Row>
-                            <h2>Logo</h2>
-                        </Row>
-                    </Footer>
-                </Layout>
-            </div>
+            
             : <NotAuthorized/>
     );
 }
