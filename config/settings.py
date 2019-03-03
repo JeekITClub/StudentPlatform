c4ca90e3a4af8baf943fa5866eecf24fdef6c657
Ni_Chujie <ncj19991213@126.com>
2019-03-03 16:59:50 +0800

    update settings

--- config/settings.py
+++ config/settings.py
@@ -131,7 +131,7 @@ STATICFILES_DIRS = [
 
 STATIC_ROOT = os.path.join(BASE_DIR, "collectstatic")
 
-MEDIA_ROOT = os.path.join(BASE_DIR, "jpsp")
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 MEDIA_URL = '/media/'
 
