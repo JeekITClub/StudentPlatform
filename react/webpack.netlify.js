09bbd07f5e1a8b82861446e3a16f02580004a05a
Harvey Qiu <qiushimao@hotmail.com>
2019-05-13 20:23:51 -0500

    add config for netlify (#191)

---
+++ react/webpack.netlify.js
@@ -0,0 +1,22 @@
+const path = require('path');
+const merge = require('webpack-merge');
+const config = require('./webpack.config');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = merge(config, {
+    mode: "production",
+    output: {
+        filename: '[name].[hash].js',
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: "[name].[hash].css",
+            chunkFilename: "[id].css"
+        }),
+        new HtmlWebPackPlugin({
+            template: "./template.html",
+            filename: path.resolve(__dirname, '../templates/react/index.html')
+        })
+    ],
+});
