c293636f6b7006cfa2801cc14a07b46c7232f46b
HarveyQiu <qiushimao@hotmail.com>
2020-01-02 08:34:31 +0800

    add createStudent mutation

--- graphqlapi/query.py
+++ graphqlapi/query.py
@@ -1,15 +1,40 @@
 from graphene_django import DjangoObjectType
 import graphene
 from student.models import Student
+from django.contrib.auth.models import User
 
 
-class StudentModel(DjangoObjectType):
+class StudentType(DjangoObjectType):
     class Meta:
         model = Student
 
 
 class Query(graphene.ObjectType):
-    students = graphene.List(StudentModel)
+    students = graphene.List(StudentType)
 
     def resolve_students(self, info):
         return Student.objects.all()
+
+class StudentMutation(graphene.Mutation):
+    class Arguments:
+        # The input arguments for this mutation
+        username = graphene.String(required=True)
+
+    # The class attributes define the response of the mutation
+    student = graphene.Field(StudentType)
+
+    def mutate(self, info, username):
+        user = User.objects.create_user(username=username, password='12331' + 'ncjnb')
+        student = Student.objects.create(
+            user=user,
+            name='123',
+            grade=1,
+            class_num=1,
+            qq='123'
+        )
+        # Notice we return an instance of this mutation
+        return StudentMutation(student=student)
+
+
+class Mutation(graphene.ObjectType):
+    create_student = StudentMutation.Field()
--- graphqlapi/schema.py
+++ graphqlapi/schema.py
@@ -1,4 +1,4 @@
-from graphqlapi.query import Query
+from graphqlapi.query import Query,Mutation
 import graphene
 
-schema = graphene.Schema(query=Query)
+schema = graphene.Schema(query=Query,mutation=Mutation)
